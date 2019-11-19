16f420a4131a9669515499a7da17e4e53b4e7a34
Jack Wu <wj@dofun.cc>
2019-11-19 17:50:39 +0800

    添加平台 SOA 权限单点登录模块

---
+++ plat-soa/src/main/java/club/codenow/plat/soa/PlatSoaApplication.java
@@ -0,0 +1,13 @@
+package club.codenow.plat.soa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatSoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatSoaApplication.class, args);
+    }
+
+}
---
+++ plat-soa/src/test/java/club/codenow/plat/soa/PlatSoaApplicationTests.java
@@ -0,0 +1,15 @@
+package club.codenow.plat.soa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlatSoaApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+
+}
